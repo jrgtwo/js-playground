ee763f1b5404c3f2b6df2140eeb778b3853c8a4b
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2024-04-27 08:14:03 -0700

    better combinationSum

--- array/combinationSum.js
+++ array/combinationSum.js
@@ -1,44 +1,72 @@
 
-const combinationSum = function (candidates, target) {
+// const combinationSum = function (candidates, target) {
 
-  candidates.sort((a, b) => a - b)
-  let idx = 0
-  let right = idx + 1
-  let store = 0
-  let endStack = []
-  let currCount = 0
-  let currStack = []
+//   candidates.sort((a, b) => a - b)
+//   let idx = 0
+//   let right = idx + 1
+//   let store = 0
+//   let endStack = []
+//   let currCount = 0
+//   let currStack = []
 
-  while (idx < candidates.length) {
+//   while (idx < candidates.length) {
 
-    if (candidates[idx] > target) break
-    if (candidates[idx] === target) break
+//     if (candidates[idx] > target) break
+//     //if (candidates[idx] === target) break
 
-    if (candidates[idx] + currCount === target) {
-      currStack.push(candidates[idx])
-      endStack.push([...currStack])
+//     if (candidates[idx] === target, candidates[idx] + currCount === target) {
+//       currStack.push(candidates[idx])
+//       endStack.push([...currStack])
 
-      if (currStack[0] === currStack[currStack.length - 1]) {
-        idx++
-      }
-      currCount = 0
-      currStack = []
+//       if (currStack[0] === currStack[currStack.length - 1]) {
+//         const val = currStack.shift()
+//         currCount -= val
+//         idx++
+//       } else {
+//         currCount = 0
+//         currStack = []
+//       }
 
-    } else if (candidates[idx] + currCount < target) {
-      currStack.push(candidates[idx])
-      currCount = currCount + candidates[idx]
-    } else {
-      if (currStack.length > 0) {
-        const lastVal = currStack.pop()
-        currCount -= lastVal
 
-      }
-      idx++
+//     } else if (candidates[idx] + currCount < target) {
+//       currStack.push(candidates[idx])
+//       currCount = currCount + candidates[idx]
+//     } else {
+//       if (currStack.length > 0) {
+//         const lastVal = currStack.pop()
+//         currCount -= lastVal
+//       }
+//       idx++
+//     }
+
+//   }
+
+//   return endStack
+// };
+
+const combinationSum = (candidates, target) => {
+  let result = []
+
+  const dfs = (i, curr, total) => {
+    console.log(i, curr, total)
+    if (total === target) {
+      result.push([...curr])
+      return
+    }
+    if ((i >= candidates.length) || total > target) {
+      return
     }
 
+    curr.push(candidates[i])
+
+    dfs(i, curr, total + candidates[i])
+    curr.pop()
+    dfs(i + 1, curr, total)
   }
 
-  return endStack
-};
+  dfs(0, [], 0)
+  return result
+}
 
-console.log(combinationSum([2, 3, 6, 7], 9))
+
+console.log(combinationSum([2, 3, 6, 7], 7))
