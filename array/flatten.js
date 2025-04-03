37c8bf045157a87638572ef496c89f696566a3f6
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-04-02 20:52:12 -0700

    fix: additional test for flatten

--- array/flatten.js
+++ array/flatten.js
@@ -2,6 +2,10 @@ const testData = [
   1, 2, 3, [4, 5, 6], 7, [[8], [9, 10, 11], 12], 13
 ]
 
+const testData2 = [
+  1, 2, 3, [4, [5, [6, 7], 8], 9], 10, [[11], [12, 13, { 14: 15 }], 16], 17
+]
+
 const toFlatten = () => {
 
 }
@@ -19,19 +23,17 @@ const toFlatten = () => {
 //   })
 
 // }
+// console.log(output)
 
-const flatten = (data) => {
-  const flattened = data.reduce((acc, elem) => {
-    if (Array.isArray(elem)) {
-      return [...acc, ...flatten(elem)]
-    }
-    return [...acc, elem]
-  }, [])
+const flatten = (data) => data.reduce((acc, elem) => {
+  if (Array.isArray(elem)) return [...acc, ...flatten(elem)]
 
-  return flattened
-}
+  return [...acc, elem]
+}, [])
 
 console.log(testData)
 console.log(flatten(testData))
+console.log('=======')
+console.log(testData2)
+console.log(flatten(testData2))
 
-// console.log(output)
