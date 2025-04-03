b120be94581a36d8c17966dbb37a7d7ff82a7247
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-04-02 20:46:08 -0700

    fix: better approach to flatten

--- array/flatten.js
+++ array/flatten.js
@@ -2,18 +2,36 @@ const testData = [
   1, 2, 3, [4, 5, 6], 7, [[8], [9, 10, 11], 12], 13
 ]
 
-const output = []
+const toFlatten = () => {
 
-const flatten = (data) => {
-  data.forEach((item) => {
-    if (Array.isArray(item)) {
-      flatten(item)
-    } else {
-      output.push(item)
-    }
-  })
 }
 
-flatten(testData)
 
-console.log(output)
+
+// const flatten = (data) => {
+//   const output = []
+//   data.forEach((item) => {
+//     if (Array.isArray(item)) {
+//       flatten(item)
+//     } else {
+//       output.push(item)
+//     }
+//   })
+
+// }
+
+const flatten = (data) => {
+  const flattened = data.reduce((acc, elem) => {
+    if (Array.isArray(elem)) {
+      return [...acc, ...flatten(elem)]
+    }
+    return [...acc, elem]
+  }, [])
+
+  return flattened
+}
+
+console.log(testData)
+console.log(flatten(testData))
+
+// console.log(output)
